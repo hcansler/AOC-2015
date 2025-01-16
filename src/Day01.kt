5492311472b7fc016c81cad2c96bd62f35a69aed
Hillary Cansler <hillary.cansler@trillianthealth.com>
2025-01-16 16:48:03 -0600

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,25 +1,36 @@
 fun main() {
-    val testInput = readInput("input_test")
-    val input = readInput("input")
+//    val testInput = readInput("input_test")
+    val input = readInput("Day01")
 
     val solution = Day01()
 
-    println(solution.part1(testInput))
+    println(input.first().toList().size)
+    println(solution.part2(input))
 
-//    check(solution.part1(testInput) == 0)
-//    check(solution.part1(input) == 0)
+//    check(solution.part1(input) == 232)
 //
-//    check(solution.part2(testInput) == 0)
-//    check(solution.part2(input) == 0)
+//    check(solution.part2(input) == 1783)
 
 }
 
 class Day01 {
     fun part1(input: List<String>): Int {
-        return 0
+        val directions = input.first().toList()
+        return directions.count { it == '(' } - directions.count { it == ')' }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val directions = input.first().toList()
+        var currentFloor = 0
+        var position = 0
+        for (i in directions.indices) {
+            if (directions[i] == '(') currentFloor ++
+            else currentFloor --
+            if (currentFloor == -1) {
+                position = i + 1
+                break
+            }
+        }
+        return position
     }
 }
